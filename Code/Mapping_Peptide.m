8f7bb09ddec215f334bbcd536072e93d934e022f
Mercy <Mercy@host174-118.vpn.ubc.ca>
2015-12-07 15:43:02 -0500

    Make Mapping_Peptide.m

---
+++ Code/Mapping_Peptide.m
@@ -0,0 +1,33 @@
+%%%%%%%%%%%%%%% Instructions for running Gauss_Build.m:
+%
+%
+%%%%%%%%%%%%%%% Logic:
+% This script just makes figures (at least for now). It fills in the gap between raw instrument data
+% and identified proteins; it looks at "what's going on with the peptides?"
+%
+%
+%%%%%%%%%%%%%%% Custom functions called by Gauss_build.m:
+%
+%
+%%%%%%%%%%%%%%% To do:
+%
+%
+%%%%%%%%%%%%%%% Fix the logic:
+%
+%
+%%%%%%%%%%%%%%% Bugs in my (this) code:
+%
+%
+%%%%%%%%%%%%%%% Questions:
+% - Where do the input files come from, e.g. 'UP_human_canonical_isoforms_TrEMBL_Oct2013.fasta' and
+%   'modificationSpecificPeptides.txt'?
+%   - fasta file comes from uniprot
+%   - the txt file might be output from maxquant (?)
+% - What is 'proteinGroups.txt'?
+%   - MQ output
+% - What are "peptide results", e.g. from "read in peptide results"?
+%   - a huge MQ output file. This is split up into chunks to make for easier reading.
+% - Why do you need to process HvsL and MvsL if you're processing HvsM?
+% - Why are most of the figures 'HvsM'?
+% - How does the fasta file come in?
+%
