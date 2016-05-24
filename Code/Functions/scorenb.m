54a18932eb5b44f6e8db7c58f14a2415401584ac
Greg Stacey <Mercy@host73-115.vpn.ubc.ca>
2016-05-24 16:07:57 -0700

    "Increase training length to 8000"

--- Code/Functions/scorenb.m
+++ Code/Functions/scorenb.m
@@ -32,7 +32,7 @@ end
 
 % what training length do we need to expect to get 5 class == 1?
 classratio = sum(y==1) / sum(y==-1);
-trainingLength = max([1000 round(5/classratio)]);
+trainingLength = max([8000 round(5/classratio)]);
 
 Nmodel = 15;
 score = nan(size(X,1),Nmodel);
@@ -40,30 +40,17 @@ feats = nan(Nmodel,size(X,2));
 for iter = 1:Nmodel
   % Make training and testing data
   
-  % balance training data
-  %   I1 = find(y==1 & I);
-  %   I1 = I1(randsample(length(I1),round(trainingLength/2)));
-  %   I0 = find(y==-1);
-  %   I0 = I0(randsample(length(I0),round(trainingLength/2)));
-  %   Itrain = ismember(1:length(y),[I1;I0]);
-  
-  % non-balanced training data
-  go = 1;
-  train_iter = 0;
-  iterMax = 1000;
-  while go
-    train_iter = train_iter+1;
-    Iall = find(I);
-    Iall = Iall(randsample(length(Iall),trainingLength));
-    Itrain = ismember(1:length(y),Iall);
-    
+  if 0
+    % balance training data
+    I1 = find(y==1 & I);
+    I1 = I1(randsample(length(I1),round(trainingLength/2)));
+    I0 = find(y==-1);
+    I0 = I0(randsample(length(I0),round(trainingLength/2)));
+    Itrain = ismember(1:length(y),[I1;I0]);
     Ipred = ~Itrain;
     Xtr = X(Itrain,:);
     ytr = y(Itrain);
     Xnew = X(Ipred,:);
-    
-    % Ensure there are at least 3 data points in each class
-    go1 = sum(ytr>0)<2 | sum(ytr<0)<2;
     
     % Ensure no class-variable pair has zero variance
     testvar = nan(2,Nd);
@@ -71,9 +58,36 @@ for iter = 1:Nmodel
       testvar(1,jj) = var(Xtr(ytr>0,jj));
       testvar(2,jj) = var(Xtr(ytr<0,jj));
     end
-    go2 = sum(testvar(1,:)~=0) == 0 | sum(testvar(2,:)~=0) == 0;
     
-    go = (go1 | go2) & train_iter<=iterMax;
+  else
+    % non-balanced training data
+    go = 1;
+    train_iter = 0;
+    iterMax = 1000;
+    while go
+      train_iter = train_iter+1;
+      Iall = find(I);
+      Iall = Iall(randsample(length(Iall),trainingLength));
+      Itrain = ismember(1:length(y),Iall);
+      
+      Ipred = ~Itrain;
+      Xtr = X(Itrain,:);
+      ytr = y(Itrain);
+      Xnew = X(Ipred,:);
+      
+      % Ensure there are at least 3 data points in each class
+      go1 = sum(ytr>0)<2 | sum(ytr<0)<2;
+      
+      % Ensure no class-variable pair has zero variance
+      testvar = nan(2,Nd);
+      for jj = 1:Nd
+        testvar(1,jj) = var(Xtr(ytr>0,jj));
+        testvar(2,jj) = var(Xtr(ytr<0,jj));
+      end
+      go2 = sum(testvar(1,:)~=0) == 0 | sum(testvar(2,:)~=0) == 0;
+      
+      go = (go1 | go2) & train_iter<=iterMax;
+    end
   end
   
   
@@ -86,7 +100,7 @@ for iter = 1:Nmodel
   if isempty(f2consider)
     f2consider = find(testvar(1,:)>0 & testvar(2,:)>0 );
   end
-    
+  
   % Fit Naive Bayes model
   nab = fitcnb(Xtr(:,f2consider),ytr);
   [~,scoretmp] = predict(nab,Xnew(:,f2consider));
