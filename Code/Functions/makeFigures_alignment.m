756c3d4321d92bf328161aa417eee8e9bed0dd5c
Greg Stacey <Mercy@host73-115.vpn.ubc.ca>
2016-05-24 16:06:39 -0700

    "Silence most of the histograms"

--- Code/Functions/makeFigures_alignment.m
+++ Code/Functions/makeFigures_alignment.m
@@ -57,104 +57,104 @@ saveas(Image, Image_name)
 
 
 
-%% Divergence histograms
-
-raw_bin_values=1:50;
-
-for ci = 1:Nchannels
-  figure
-  
-  for ii = 1:Nreplicates
-    subplot(Nreplicates,1,ii)
-    raw_divergent=abs(Delta_height{ci,replicate_to_align_against(ci),ii}) ...
-      + abs(Delta_width{ci,replicate_to_align_against(ci),ii})...
-      + abs(Delta_center{ci,replicate_to_align_against(ci),ii});
-    hist(raw_divergent,raw_bin_values)
-    title('Divergence between rep1 and rep2')
-    xlabel('Divergence');
-    ylabel('Number of identifications');
-  end
-  
-  %   subplot(2,2,2)
-  %   raw_divergent=abs(Delta_height{ci,1,3})+abs(Delta_width{ci,1,3})+abs(Delta_center{ci,1,3});
-  %   hist(raw_divergent,raw_bin_values)
-  %   title('Divergence between rep1 and rep2')
-  %   xlabel('Divergence');
-  %   ylabel('Number of identifications');
-  %
-  %   subplot(2,2,3)
-  %   raw_divergent=abs(Delta_height{ci,3,2})+abs(Delta_width{ci,3,2})+abs(Delta_center{ci,3,2});
-  %   hist(raw_divergent,raw_bin_values)
-  %   title('Divergence between rep1 and rep2')
-  %   xlabel('Divergence');
-  %   ylabel('Number of identifications');
-  
-  Image_name=[figdir 'DivergenceHist_between_replicates' Experimental_channels{ci} '.png'];
-  saveas(gcf, Image_name, 'png');
-end
-
-
-
-%% Delta_C, _H, _W, _Euc histogram
-
-comparisons = nan(Nreplicates,2);
-for ii = 1:Nreplicates
-  comparisons(ii,:) = [replicate_to_align_against(ci) ii];
-end
-
-for ci = 1:Nchannels
-  for rr = 1:size(comparisons,1)
-    rr1 = comparisons(rr,1);
-    rr2 = comparisons(rr,2);
-    
-    %Hist of Center, height and width widthin user defined setting
-    fh2 = figure; % open figure tablet to create figure on
-    
-    %hist for height
-    P1=subplot(4,1,1); hold on
-    raw_bin_height_values_user=0.1:0.1:7.5; % bin values can be changed by the user to improve figure quility
-    Histc_height=histc(Delta_height{ci,rr1,rr2},raw_bin_height_values_user);
-    bar(raw_bin_height_values_user,Histc_height); %crete bar graph of values
-    y=ylim;
-    plot([1 1].*prctile(Delta_height{ci,rr1,rr2},90),y,'k')
-    xlabel(P1,'Height');
-    ylabel(P1,'# of Gaussians');
-    
-    %hist for center
-    P2=subplot(4,1,2); hold on
-    raw_bin_center_values_user=0.1:0.1:4.5;
-    Histc_center=histc(Delta_center{ci,rr1,rr2},raw_bin_center_values_user);
-    bar(raw_bin_center_values_user,Histc_center);
-    y=ylim;
-    plot([1 1].*prctile(Delta_center{ci,rr1,rr2},90),y,'k')
-    xlabel(P2,'Center');
-    ylabel(P2,'# of Gaussians');
-    
-    %hist for width
-    P3=subplot(4,1,3); hold on
-    raw_bin_center_values_user=0.1:0.1:4.5;
-    Histc_center=histc(Delta_width{ci,rr1,rr2},raw_bin_center_values_user);
-    bar(raw_bin_center_values_user,Histc_center);
-    y=ylim;
-    plot([1 1].*prctile(Delta_width{ci,rr1,rr2},90),y,'k')
-    xlabel(P3,'Width');
-    ylabel(P3,'# of Gaussians');
-    
-    %hist for Euclidean distance
-    P4 = subplot(4,1,4); hold on
-    Euclidean_distance_maximum_value=max(EuDis{ci,rr1,rr2});
-    raw_bin_Euclidean_distance_values_user=0.5:0.5:Euclidean_distance_maximum_value;
-    Histc_Euclidean_distance=histc(EuDis{ci,rr1,rr2},raw_bin_Euclidean_distance_values_user);
-    bar(raw_bin_Euclidean_distance_values_user,Histc_Euclidean_distance);
-    y=ylim;
-    plot([1 1].*prctile(EuDis{ci,rr1,rr2},90),y,'k')
-    xlabel(P4,'Euclidean Distance');
-    ylabel(P4,'# of Gaussians');
-    
-    Image_name=[figdir 'Histograms_rep' num2str(rr1) '_vs_rep' num2str(rr2) '_' Experimental_channels{ci} '.png'];
-    saveas(gcf, Image_name, 'png');
-  end
-end
+% % Divergence histograms
+% 
+% raw_bin_values=1:50;
+% 
+% for ci = 1:Nchannels
+%   figure
+%   
+%   for ii = 1:Nreplicates
+%     subplot(Nreplicates,1,ii)
+%     raw_divergent=abs(Delta_height{ci,replicate_to_align_against(ci),ii}) ...
+%       + abs(Delta_width{ci,replicate_to_align_against(ci),ii})...
+%       + abs(Delta_center{ci,replicate_to_align_against(ci),ii});
+%     hist(raw_divergent,raw_bin_values)
+%     title('Divergence between rep1 and rep2')
+%     xlabel('Divergence');
+%     ylabel('Number of identifications');
+%   end
+%   
+%     subplot(2,2,2)
+%     raw_divergent=abs(Delta_height{ci,1,3})+abs(Delta_width{ci,1,3})+abs(Delta_center{ci,1,3});
+%     hist(raw_divergent,raw_bin_values)
+%     title('Divergence between rep1 and rep2')
+%     xlabel('Divergence');
+%     ylabel('Number of identifications');
+%   
+%     subplot(2,2,3)
+%     raw_divergent=abs(Delta_height{ci,3,2})+abs(Delta_width{ci,3,2})+abs(Delta_center{ci,3,2});
+%     hist(raw_divergent,raw_bin_values)
+%     title('Divergence between rep1 and rep2')
+%     xlabel('Divergence');
+%     ylabel('Number of identifications');
+%   
+%   Image_name=[figdir 'DivergenceHist_between_replicates' Experimental_channels{ci} '.png'];
+%   saveas(gcf, Image_name, 'png');
+% end
+% 
+% 
+% 
+% % Delta_C, _H, _W, _Euc histogram
+% 
+% comparisons = nan(Nreplicates,2);
+% for ii = 1:Nreplicates
+%   comparisons(ii,:) = [replicate_to_align_against(ci) ii];
+% end
+% 
+% for ci = 1:Nchannels
+%   for rr = 1:size(comparisons,1)
+%     rr1 = comparisons(rr,1);
+%     rr2 = comparisons(rr,2);
+%     
+%     Hist of Center, height and width widthin user defined setting
+%     fh2 = figure; % open figure tablet to create figure on
+%     
+%     hist for height
+%     P1=subplot(4,1,1); hold on
+%     raw_bin_height_values_user=0.1:0.1:7.5; % bin values can be changed by the user to improve figure quility
+%     Histc_height=histc(Delta_height{ci,rr1,rr2},raw_bin_height_values_user);
+%     bar(raw_bin_height_values_user,Histc_height); %crete bar graph of values
+%     y=ylim;
+%     plot([1 1].*prctile(Delta_height{ci,rr1,rr2},90),y,'k')
+%     xlabel(P1,'Height');
+%     ylabel(P1,'# of Gaussians');
+%     
+%     hist for center
+%     P2=subplot(4,1,2); hold on
+%     raw_bin_center_values_user=0.1:0.1:4.5;
+%     Histc_center=histc(Delta_center{ci,rr1,rr2},raw_bin_center_values_user);
+%     bar(raw_bin_center_values_user,Histc_center);
+%     y=ylim;
+%     plot([1 1].*prctile(Delta_center{ci,rr1,rr2},90),y,'k')
+%     xlabel(P2,'Center');
+%     ylabel(P2,'# of Gaussians');
+%     
+%     hist for width
+%     P3=subplot(4,1,3); hold on
+%     raw_bin_center_values_user=0.1:0.1:4.5;
+%     Histc_center=histc(Delta_width{ci,rr1,rr2},raw_bin_center_values_user);
+%     bar(raw_bin_center_values_user,Histc_center);
+%     y=ylim;
+%     plot([1 1].*prctile(Delta_width{ci,rr1,rr2},90),y,'k')
+%     xlabel(P3,'Width');
+%     ylabel(P3,'# of Gaussians');
+%     
+%     hist for Euclidean distance
+%     P4 = subplot(4,1,4); hold on
+%     Euclidean_distance_maximum_value=max(EuDis{ci,rr1,rr2});
+%     raw_bin_Euclidean_distance_values_user=0.5:0.5:Euclidean_distance_maximum_value;
+%     Histc_Euclidean_distance=histc(EuDis{ci,rr1,rr2},raw_bin_Euclidean_distance_values_user);
+%     bar(raw_bin_Euclidean_distance_values_user,Histc_Euclidean_distance);
+%     y=ylim;
+%     plot([1 1].*prctile(EuDis{ci,rr1,rr2},90),y,'k')
+%     xlabel(P4,'Euclidean Distance');
+%     ylabel(P4,'# of Gaussians');
+%     
+%     Image_name=[figdir 'Histograms_rep' num2str(rr1) '_vs_rep' num2str(rr2) '_' Experimental_channels{ci} '.png'];
+%     saveas(gcf, Image_name, 'png');
+%   end
+% end
 
 
 
